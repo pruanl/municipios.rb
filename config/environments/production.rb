069eaf27f06287e1437ab87c035c43d2202469ad
Pedro Lima <pruan199@gmail.com>
2024-01-13 15:34:49 -0300

    adicionanod hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+  config.hosts << "municipios-rb-dev-hpjg.4.us-1.fl0.io"
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
